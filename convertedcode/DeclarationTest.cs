f1049b6b48518adc7ab8cbfdd1b505c6d09d8bc0
Martin <mjch17@student.aau.dk>
2019-05-21 10:48:23 +0200

    Added writelines to tests for testing purposes

--- convertedcode/DeclarationTest.cs
+++ convertedcode/DeclarationTest.cs
@@ -9,10 +9,19 @@ double abc2 = -13 ;
 string cde = "Declaration Test" ;
 Boolean fgh = true ;
 Boolean ijk = false ;
+Console.WriteLine ( abc ) ;
+Console.WriteLine ( abc2 ) ;
+Console.WriteLine ( cde ) ;
+Console.WriteLine ( fgh ) ;
+Console.WriteLine ( ijk ) ;
 abc = 15 ;
 cde = "Assignment Test" ;
 fgh = false ;
 ijk = true ;
+Console.WriteLine ( abc ) ;
+Console.WriteLine ( cde ) ;
+Console.WriteLine ( fgh ) ;
+Console.WriteLine ( ijk ) ;
 }
  
 } 
